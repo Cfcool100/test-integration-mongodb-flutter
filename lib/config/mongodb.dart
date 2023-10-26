f4eeb9612989e7b9e96b26ecf96d6e92861db45d
Cfcool100 <cfcool100@gmail.com>
2023-10-26 23:39:58 +0000

    replace constant by env config

--- lib/config/mongodb.dart
+++ lib/config/mongodb.dart
@@ -2,17 +2,17 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:mongo_dart/mongo_dart.dart';
-import 'package:mongodb_test/config/constant.dart';
+import 'package:mongodb_test/config/enrironment.dart';
 import 'package:mongodb_test/models/categories.model.dart';
 
 class MongoDataBase {
   static dynamic db, userCollection;
 
   static connect() async {
-    db = await Db.create(uri);
+    db = await Db.create(Env.uri);
     await db.open();
     inspect(db);
-    userCollection = await db.collection(collection);
+    userCollection = await db.collection(Env.collectionName);
   }
 
   static Future<List<Map<String, dynamic>>> fetchData() async {
