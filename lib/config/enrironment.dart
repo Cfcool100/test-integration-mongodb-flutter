99c70dc93ea617593345670c8a292fa3b47b4fd9
Cfcool100 <cfcool100@gmail.com>
2023-10-27 11:24:44 +0000

    condition mode + rename get collection

--- lib/config/enrironment.dart
+++ lib/config/enrironment.dart
@@ -1,15 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Env {
-  static String get filenName {
-    return '.env';
+  static String get fileName {
+    if (kReleaseMode) {
+      return '.env.product';
+    }
+    return '.env.development';
   }
 
   static String get uri {
     return dotenv.env['URI'] ?? 'URI don\'t exist';
   }
 
-  static String get collectionName {
+  static String get collection {
     return dotenv.env['COLLECTION_NAME'] ?? 'COLLECTION_NAME don\'t exist';
   }
 }
